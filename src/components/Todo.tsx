ee3ce23bb4b3d83896846f6dffeb8dc173e0841e
Shaiah Emigh-Doyle <sde2111@columbia.edu>
2021-06-12 11:16:09 -0400

    refactor for functional component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import React from "react";
 
 interface ITodoProps {
-    name: string;
+  name: string;
 }
-interface ITodoState {
-    isFinished: boolean;
-}
-export class Todo extends React.Component <ITodoProps> {
-    constructor(props: ITodoProps) {
-        super(props);
-        this.state = {
-            isFinished: false,
-        }
-    }
-    render() {
-        return (
-<div>{this.props.name}</div>
-        )
-    }
-}
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export const FunctionalTodo: React.FC<ITodoProps> = (props) => {
+  const [isFinished, setIsFinished] = React.useState<boolean>(false);
+  const classes = useStyles();
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="body2" component="p">
+            {isFinished ? <del>{props.name}</del> : props.name}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button onClick={() => setIsFinished(!isFinished)} size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
